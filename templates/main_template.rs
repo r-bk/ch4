95769b5471ca8c114250559761633e87444120e6
Rafael Buchbinder <rafi@rbk.io>
2021-08-07 10:06:47 +0300

    main: coding: remove redundant conditionals
    The template is much more readable now.

--- templates/main_template.rs
+++ templates/main_template.rs
@@ -4,6 +4,8 @@ use rsdns::{constants::{RClass, RType}, records::data::{self, RData}};
 use std::{fmt::Write, time::SystemTime};
 
 {% if async == "true" %}
+{% set as = "async"  %}
+{% set aw = ".await" %}
 cfg_if::cfg_if! {
     if #[cfg(feature = "net-tokio")] {
         use rsdns::resolvers::tokio::Resolver;
@@ -16,14 +18,16 @@ cfg_if::cfg_if! {
     }
 }
 {% else %}
+{% set as = "" %}
+{% set aw = "" %}
 use rsdns::resolvers::std::Resolver;
 {% endif %}
 
-{% if async == "true" %}async{% endif %} fn query_rrset_impl<D: RData>(r: &mut Resolver, qname: &str) -> Result<()>
+{{ as }} fn query_rrset_impl<D: RData>(r: &mut Resolver, qname: &str) -> Result<()>
 where
     rdata::RDataFmt: rdata::RDataFormatter<String, D>
 {
-    let rrset = r.query_rrset::<D>(qname, RClass::In){% if async == "true" %}.await{% endif %}?;
+    let rrset = r.query_rrset::<D>(qname, RClass::In){{ aw }}?;
     let mut buf = String::new();
     writeln!(&mut buf, "QName: {}", qname)?;
     rrset::fmt(&mut buf, &rrset)?;
@@ -32,42 +36,42 @@ where
 }
 
 
-{% if async == "true" %}async{% endif %} fn query_rrset(r: &mut Resolver, qtype: RType, qname: &str) -> Result<()> {
+{{ as }} fn query_rrset(r: &mut Resolver, qtype: RType, qname: &str) -> Result<()> {
     match qtype {
-        RType::A => query_rrset_impl::<data::A>(r, qname){% if async == "true" %}.await{% endif %},
-        RType::Ns => query_rrset_impl::<data::Ns>(r, qname){% if async == "true" %}.await{% endif %},
-        RType::Md => query_rrset_impl::<data::Md>(r, qname){% if async == "true" %}.await{% endif %},
-        RType::Mf => query_rrset_impl::<data::Mf>(r, qname){% if async == "true" %}.await{% endif %},
-        RType::Cname => query_rrset_impl::<data::Cname>(r, qname){% if async == "true" %}.await{% endif %},
-        RType::Soa => query_rrset_impl::<data::Soa>(r, qname){% if async == "true" %}.await{% endif %},
-        RType::Mb => query_rrset_impl::<data::Mb>(r, qname){% if async == "true" %}.await{% endif %},
-        RType::Mg => query_rrset_impl::<data::Mg>(r, qname){% if async == "true" %}.await{% endif %},
-        RType::Mr => query_rrset_impl::<data::Mr>(r, qname){% if async == "true" %}.await{% endif %},
-        RType::Null => query_rrset_impl::<data::Null>(r, qname){% if async == "true" %}.await{% endif %},
-        RType::Wks => query_rrset_impl::<data::Wks>(r, qname){% if async == "true" %}.await{% endif %},
-        RType::Ptr => query_rrset_impl::<data::Ptr>(r, qname){% if async == "true" %}.await{% endif %},
-        RType::Hinfo => query_rrset_impl::<data::Hinfo>(r, qname){% if async == "true" %}.await{% endif %},
-        RType::Minfo => query_rrset_impl::<data::Minfo>(r, qname){% if async == "true" %}.await{% endif %},
-        RType::Mx => query_rrset_impl::<data::Mx>(r, qname){% if async == "true" %}.await{% endif %},
-        RType::Txt => query_rrset_impl::<data::Txt>(r, qname){% if async == "true" %}.await{% endif %},
-        RType::Aaaa => query_rrset_impl::<data::Aaaa>(r, qname){% if async == "true" %}.await{% endif %},
+        RType::A => query_rrset_impl::<data::A>(r, qname){{ aw }},
+        RType::Ns => query_rrset_impl::<data::Ns>(r, qname){{ aw }},
+        RType::Md => query_rrset_impl::<data::Md>(r, qname){{ aw }},
+        RType::Mf => query_rrset_impl::<data::Mf>(r, qname){{ aw }},
+        RType::Cname => query_rrset_impl::<data::Cname>(r, qname){{ aw }},
+        RType::Soa => query_rrset_impl::<data::Soa>(r, qname){{ aw }},
+        RType::Mb => query_rrset_impl::<data::Mb>(r, qname){{ aw }},
+        RType::Mg => query_rrset_impl::<data::Mg>(r, qname){{ aw }},
+        RType::Mr => query_rrset_impl::<data::Mr>(r, qname){{ aw }},
+        RType::Null => query_rrset_impl::<data::Null>(r, qname){{ aw }},
+        RType::Wks => query_rrset_impl::<data::Wks>(r, qname){{ aw }},
+        RType::Ptr => query_rrset_impl::<data::Ptr>(r, qname){{ aw }},
+        RType::Hinfo => query_rrset_impl::<data::Hinfo>(r, qname){{ aw }},
+        RType::Minfo => query_rrset_impl::<data::Minfo>(r, qname){{ aw }},
+        RType::Mx => query_rrset_impl::<data::Mx>(r, qname){{ aw }},
+        RType::Txt => query_rrset_impl::<data::Txt>(r, qname){{ aw }},
+        RType::Aaaa => query_rrset_impl::<data::Aaaa>(r, qname){{ aw }},
         RType::Axfr | RType::Mailb | RType::Maila | RType::Any => bail!("invalid qtype"),
     }
 }
 
-pub {% if async == "true" %}async{% endif %} fn main() -> Result<()> {
+pub {{ as }} fn main() -> Result<()> {
     let mut buf = [0u8; u16::MAX as usize];
 
     let args = Args::get();
     let (conf, qtype, qnames) = args.parse()?;
 
-    let mut resolver = Resolver::new(conf.clone()){% if async == "true" %}.await{% endif %}?;
+    let mut resolver = Resolver::new(conf.clone()){{ aw }}?;
 
     for (index, qname) in qnames.iter().enumerate() {
         if !args.rrset {
             let now = SystemTime::now();
             let size = resolver
-                .query_raw(qname, qtype, RClass::In, &mut buf){% if async == "true" %}.await{% endif %}?;
+                .query_raw(qname, qtype, RClass::In, &mut buf){{ aw }}?;
             let elapsed = now.elapsed().expect("time failed");
 
             let output = Output::new(&args, qname, qtype, &buf[..size], now, elapsed, &conf);
@@ -76,7 +80,7 @@ pub {% if async == "true" %}async{% endif %} fn main() -> Result<()> {
                 println!();
             }
         } else {
-            query_rrset(&mut resolver, qtype, qname){% if async == "true" %}.await{% endif %}?;
+            query_rrset(&mut resolver, qtype, qname){{ aw }}?;
         }
     }
 
